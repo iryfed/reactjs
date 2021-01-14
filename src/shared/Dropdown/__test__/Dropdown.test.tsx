83a5421201c573a2a7105d193fe7fc5f675b1ae1
Irina <57986927+iryfed@users.noreply.github.com>
2021-01-14 17:56:01 +0300

    Add files via upload
    home work #6

---
+++ src/shared/Dropdown/__test__/Dropdown.test.tsx
@@ -0,0 +1,21 @@
+/**
+ * @jest-environment jsdom
+ */
+
+import React from 'react';
+import { shallow } from 'enzyme';
+import { Dropdown } from '../Dropdown';
+
+
+describe('Dropdown', () => {
+    test('should render', () => {
+        const wrapper = shallow(<Dropdown children={<div />} button={<button />}/>)
+        expect(wrapper).toBeDefined();
+        expect(wrapper.find('div.container').isEmptyRender()).toBeFalsy();
+    })
+
+    test('should render (snapshot)', () => {
+        const wrapper = shallow(<Dropdown children={<div />} button={<button />}/>)
+        expect(wrapper).toMatchSnapshot();
+    })
+})
