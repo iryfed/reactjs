d32d2aadda5e774bca3a06b36b2fadd5df5273e0
Irina <57986927+iryfed@users.noreply.github.com>
2021-02-03 16:04:22 +0300

    Add files via upload
    hw8

--- src/server/server.js
+++ src/server/server.js
@@ -2,16 +2,26 @@ import express from 'express';
 import ReactDOM from 'react-dom/server';
 import { indexTemplate } from './indexTemplate';
 import { App } from '../App';
+import axios from 'axios';
 
 const app = express();
 
 app.use('/static', express.static('./dist/client'));
 
 app.get('/auth', (req, res) => {
-  //req.query.code
-  res.send(
-    indexTemplate(ReactDOM.renderToString(App())),
-  );
+  axios.post(
+    'https://www.reddit.com/api/v1/access_token',
+    `grant_type=authorization_code&code=${req.query.code}&redirect_uri=http://localhost:3000/auth`,
+    {
+      auth: { username: 'oBtLDkWhhgV-MQ', password: 'KHc7eplg3TQ8bWfp9PNWZJZMm-6R1A' },
+      headers: { 'Content-type': 'application/x-www-form-urlencoded' }
+    }
+  ).then(({ data }) => {
+    res.send(
+      indexTemplate(ReactDOM.renderToString(App()), data['access_token']),
+    );
+  }
+  ).catch(console.log)
 });
 
 app.get('/', (req, res) => {
