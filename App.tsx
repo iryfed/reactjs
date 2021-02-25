f1ede7ab6f46a24462f054606548427c681968f5
Irina <57986927+iryfed@users.noreply.github.com>
2021-02-25 17:39:45 +0300

    Add files via upload
    home work 10

--- App.tsx
+++ App.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import './main.global.scss';
-import { useToken } from './utils/hooks/useToken';
 import { Layout } from './shared/Layout/Layout';
 import { Header } from './shared/Header/Header';
 import { Content } from './shared/Content/Content';
@@ -8,30 +7,31 @@ import { CardsList } from './shared/CardsList/CardsList';
 import { SearchBlock } from './shared/SearchBlock/SearchBlock';
 import { ThreadTitle } from './shared/ThreadTitle/ThreadTitle';
 import { SortBlock } from './shared/SortBlock/SortBlock';
-import { tokenContext } from './shared/context/tokenContext';
 import { UserContextProvider } from './shared/context/userContext';
 import { PostsContextProvider } from './shared/context/postsContext';
 
-function AppComponent() {
-  const [token] = useToken();
+import { Provider } from 'react-redux';
+import { store } from './shared/redux/store';
 
+
+function AppComponent() {
   return (
-    <tokenContext.Provider value={token}>
-    <UserContextProvider>
-    <PostsContextProvider>
-    <Layout>
-      <Header>
-        <SearchBlock />
-        <ThreadTitle />
-        <SortBlock />
-      </Header>
-      <Content>
-        <CardsList />
-      </Content>
-    </Layout>
-    </PostsContextProvider>
-    </UserContextProvider>
-    </tokenContext.Provider>
+    <Provider store={store}>
+      <UserContextProvider>
+      <PostsContextProvider>
+      <Layout>
+        <Header>
+          <SearchBlock />
+          <ThreadTitle />
+          <SortBlock />
+        </Header>
+        <Content>
+          <CardsList />
+        </Content>
+      </Layout>
+      </PostsContextProvider>
+      </UserContextProvider>
+    </Provider>
   );
 }
 
