9344ca699dc44821345119794a9fafe66dfd6360
Irina <57986927+iryfed@users.noreply.github.com>
2021-03-09 13:12:06 +0300

    Add files via upload
    home work 11

--- src/utils/hooks/usePostsData.tsx
+++ src/utils/hooks/usePostsData.tsx
@@ -1,35 +1,20 @@
-import { useEffect, useState } from 'react';
-import axios from 'axios';
+import { useEffect } from 'react';
 import { useToken } from './useToken';
 import { useDispatch, useSelector } from 'react-redux';
-import { IPostsData, PostRequest, PostRequestError, PostRequestSuccess } from '../../shared/redux/reducers/postReducer';
+import { IPostsData, postRequestAsync } from '../../shared/redux/reducers/postReducer';
 import { RootState } from '../../shared/redux/reducers/rootReducer';
 
 
 export function usePostsData() {
-  //const [data, setData] = useState<IPostsData>({});
   const data = useSelector<RootState, IPostsData>(state => state.post.data);
   const token = useToken();
   const dispatch = useDispatch();
 
   useEffect(() => {
-    if(!token || token === "undefined") return;
 
-    dispatch(PostRequest());
-    
-    axios.get(
-      'https://oauth.reddit.com/best?limit=10',
-      {
-        headers: { Authorization: `bearer ${token}`}
-      }
-    ).then((resp) => {
-      const postsData = resp.data;
-     // setData({ dist: postsData.data.dist, postsList: postsData.data.children });
-      dispatch(PostRequestSuccess({ dist: postsData.data.dist, postsList: postsData.data.children }));
-    }).catch((error) => {
-      dispatch(PostRequestError(String(error)));
-    })
-    
+    if(!token || token === "undefined") return;
+      dispatch(postRequestAsync());
+      
   }, [token])
 
   return [data]
--- src/utils/hooks/useToken.tsx
+++ src/utils/hooks/useToken.tsx
@@ -1,7 +1,7 @@
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { RootState } from '../../shared/redux/reducers/rootReducer';
-import { setToken } from '../../shared/redux/reducers/tokenReducer';
+import { saveToken } from '../../shared/redux/reducers/tokenReducer';
 
 export function useToken() {
   const token = useSelector<RootState, string>(state => state.token.token);
@@ -9,7 +9,7 @@ export function useToken() {
   
   useEffect(() => {
     if (window.__token__) {
-      dispatch(setToken(window.__token__))
+      dispatch(saveToken())
     }
   }, []);
 
